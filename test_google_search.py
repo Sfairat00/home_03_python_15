53bcc5d993b01ae907100cdbf09803720447ca3d
Semen Ozhereliew <s.ojereliew@yandex.ru>
2025-02-21 23:14:44 +0300

    Исправление замечаний

--- test_google_search.py
+++ test_google_search.py
@@ -4,8 +4,9 @@ from selene import browser, be, have
 
 @pytest.fixture(scope="session")
 def browser_context():
-    browser.config.window_height = 500
-    browser.config.window_width = 500
+    browser.config.window_height = 900
+    browser.config.window_width = 900
+
 
 @pytest.fixture(autouse=True)
 def operation_browser():
@@ -13,10 +14,12 @@ def operation_browser():
     yield
     browser.quit()
 
-def test_valid_result():
+
+def test_valid_result(browser_context):
     browser.element('[name="q"]').should(be.blank).type('yashka/selene').press_enter()
     browser.element('html').should(have.text('Об этой странице'))
 
-def test_invalid_result():
-    browser.element('[name="q"]').should(be.blank).type('ererrerrerer').press_enter()
-    browser.element('html').should(have.no.text('ничего не найдено'))
+
+def test_invalid_result(browser_context):
+    browser.element('[name="q"]').should(be.blank).type('ererrerrererенкенепвап').press_enter()
+    browser.element('html').should(have.text('По запросу ererrerrererенкенепвап ничего не найдено.'))
